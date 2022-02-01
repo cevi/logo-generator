0d12d11752ea4cebf95962f53fd42aff1f341b98
btemperli <beat@temper.li>
2022-02-01 13:54:45 +0100

    update styling of login-page

--- admin/login.php
+++ admin/login.php
@@ -16,42 +16,50 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container">
+        <div class="container container-fluid">
             <h3 class="text-center">Login</h3>
+            <div class="row justify-content-md-center">
+                <div class="col-lg-4 col-md-6">
+                    <?php
+                        if (isset($_POST['submit'])) {
 
-            <?php
-                if (isset($_POST['submit'])) {
+                            require_once('DotEnv.php');
 
-                    require_once('DotEnv.php');
+                            (new DotEnv(__DIR__ . '/.env'))->load();
 
-                    (new DotEnv(__DIR__ . '/.env'))->load();
+                            $hashed_password = getenv('ADMIN_PASSWORD_HASH');
+                            $username = $_POST['username']; $password = $_POST['password'];
+                            if ($username === 'admin' && password_verify($password , $hashed_password)) {
+                                $_SESSION['login'] = true;
+                                header('LOCATION:admin.php');
+                                die();
+                            }
 
-                    $hashed_password = getenv('ADMIN_PASSWORD_HASH');
-                    $username = $_POST['username']; $password = $_POST['password'];
-                    if ($username === 'admin' && password_verify($password , $hashed_password)) {
-                        $_SESSION['login'] = true;
-                        header('LOCATION:admin.php');
-                        die();
-                    }
+                            else {
+                                echo "<div class='alert alert-danger mt-4'>Username and Password do not match.</div>";
+                            }
 
-                    else {
-                        echo "<div class='alert alert-danger'>Username and Password do not match.</div>";
-                    }
-
-            }
-            ?>
-
-            <form action="" method="post">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" name="username" required>
+                        }
+                    ?>
                 </div>
-                <div class="form-group">
-                    <label for="pwd">Password</label>
-                    <input type="password" class="form-control" id="pwd" name="password" required>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col-lg-4 col-md-6">
+
+                    <form action="" method="post">
+                        <div class="form-group mt-2">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" id="username" name="username" required>
+                        </div>
+                        <div class="form-group mt-2">
+                            <label for="pwd">Password</label>
+                            <input type="password" class="form-control" id="pwd" name="password" required>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-default btn-primary btn-block mt-4">Login</button>
+                    </form>
                 </div>
-                <button type="submit" name="submit" class="btn btn-default">Login</button>
-            </form>
+            </div>
         </div>
     </body>
 </html>
